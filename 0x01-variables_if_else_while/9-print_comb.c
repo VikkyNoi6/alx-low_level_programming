a54ef8eacccca4813df598b21f5e8a22e4fd38ef
VikkyNoi6 <victoria.noi550@gmail.com>
2023-04-22 08:50:10 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,15 +7,17 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
+	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
-		if (n != 57)
+		if (n == 57)
 		{
-			putchar(',');
-			putchar(',');
+			continue;
 		}
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
 }
+	
